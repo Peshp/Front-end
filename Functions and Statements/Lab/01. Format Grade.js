8063ef334122e43a5c4a226f07cf0bdada382120
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-11-14 12:23:32 +0200

    Create 01. Format Grade.js

---
+++ Functions and Statements/Lab/01. Format Grade.js
@@ -0,0 +1,13 @@
+function grades(grade){
+    if(grade < 3.00){
+        console.log('Fail');
+    } else if(grade < 3.50){
+        console.log('Poor');
+    } else if(grade < 4.50){
+        console.log('Good');
+    } else if(grade < 5.50){
+        console.log('Very good');
+    } else{
+        console.log('Excellent');
+    }
+}
