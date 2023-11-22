f045ec3addac4b664e2069d7f5ffb0dc52f1c8e3
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-11-22 23:20:52 +0200

    Update SumTable.js

--- DOM Introduction/Lab/06.Sum Table/SumTable.js
+++ DOM Introduction/Lab/06.Sum Table/SumTable.js
@@ -1,3 +1,9 @@
 function sumTable() {
+    let total = 0;
+    document.querySelectorAll('table tr td:nth-child(2)')
+        .forEach((i) => {
+            total += Number(i.textContent);
+        })
 
+    document.getElementById("sum").textContent = total;
 }
