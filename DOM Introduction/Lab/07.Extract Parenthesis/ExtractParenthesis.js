21e39719013b806e94b1311d7088b8a92d156029
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-11-22 23:20:55 +0200

    Update ExtractParenthesis.js

--- DOM Introduction/Lab/07.Extract Parenthesis/ExtractParenthesis.js
+++ DOM Introduction/Lab/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,13 @@
 function extract(content) {
+    let result = [];
+    const text = document.getElementById(content).textContent;
+    let arr = text.split(" ");
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].length > 0 && arr[i][0] === '(') {
+            result.push(arr[i].substring(1, arr[i].length - 1));
+        }
+    }
 
+    return result.join("; ");
 }
+
