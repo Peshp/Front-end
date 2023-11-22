ad57140fea88c556a0af2d2da2eaf555bd8d02aa
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-11-22 23:20:40 +0200

    Update CollectListItems.js

--- DOM Introduction/Lab/02.Collect List Items/CollectListItems.js
+++ DOM Introduction/Lab/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,10 @@
 function extractText() {
-    // TODO
+    const items = Array.from(document.getElementById("items").children);
+    let extracted = [];
+
+    items.forEach((i) => {
+        extracted.push(i.textContent);
+    });
+
+    document.getElementById("result").value = extracted.join("\n");
 }
