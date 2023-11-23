f0407703cde64a8d7b366d91e7b8e95e7ce927e3
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-11-23 18:54:59 +0200

    Update EditElement.js

--- DOM Introduction/Lab/01.Edit Element/EditElement.js
+++ DOM Introduction/Lab/01.Edit Element/EditElement.js
@@ -1,3 +1,5 @@
-function editElement() {
-    let num1 =
+function editElement(ref, match, replacer) {
+    const content = ref.textContent;
+    const editedText = content.replace(new RegExp(match, 'g'), replacer);
+    ref.textContent = editedText;
 }
