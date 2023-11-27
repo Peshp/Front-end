d4100dc152caf23836bdc1cbca74f910e8dee5f1
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-11-27 13:04:24 +0200

    Update app.js

--- DOM Introduction/Exercise/07. Hell's Kitchen/app.js
+++ DOM Introduction/Exercise/07. Hell's Kitchen/app.js
@@ -1,8 +1,29 @@
-function solve() {
+function solve(inputValue) {
    document.querySelector('#btnSend').addEventListener('click', onClick);
 
-   function onClick () {
-      //   TODO:
-      
+   function onClick() {
+      let inputValue = document.getElementById('input');
+
+      const best = ' ';
+      const workers = ' ';
+
+
+      const arr =inputValue.split(',');
+      for (let i = 0; i < arr.length; i++) {
+         let newArr = arr[i].split(', ');
+
+         for (let j = 0; j < newArr.length; j++) {
+            newArr.pop();
+            if (i % 2 === 0) {
+               let newArr2 = newArr.split(' ');
+               for (let k = 0; k < newArr2; k++) {
+                  if(newArr2 % 2 === 0){
+                     avPrice += Number(newArr2)[i]);
+                  }
+
+               }
+            }
+         }
+      }
+      }
    }
-}
