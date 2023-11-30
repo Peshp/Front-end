8c8e84c0fff8d5afd95922b60388f9e321442ead
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-11-30 17:31:43 +0200

    Update app.js

--- DOM Manipulation and Events/Lab/05. Highlight-Active/app.js
+++ DOM Manipulation and Events/Lab/05. Highlight-Active/app.js
@@ -1,3 +1,12 @@
 function focused() {
-    console.log('TODO:...');
+    const elements = Array.from(document.getElementsByTagName('input'));
+    for (const element of elements) {
+        element.addEventListener('focus', (e) => {
+            e.target.parentElement.className = 'focused';
+        });
+
+        element.addEventListener('blur', (e) => {
+            e.target.parentElement.className = '';
+        })
+    }
 }
