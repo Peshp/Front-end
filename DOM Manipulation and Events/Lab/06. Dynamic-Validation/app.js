524c7355990bd3619b020303928412b62067ac4d
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-11-30 17:31:47 +0200

    Update app.js

--- DOM Manipulation and Events/Lab/06. Dynamic-Validation/app.js
+++ DOM Manipulation and Events/Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,12 @@
 function validate() {
-    console.log('TODO:...');
+    const inputField = document.getElementById('email');
+    inputField.addEventListener('change', function (e) {
+        let pattern = new RegExp('/[a-z]+@[a-z]+\.[a-z]+/g');
+        let inputText = inputField.value;
+        if(pattern.test(inputText)){
+            e.target.classList.remove('error');
+        } else{
+            e.target.classList.add('error');
+        }
+    })
 }
