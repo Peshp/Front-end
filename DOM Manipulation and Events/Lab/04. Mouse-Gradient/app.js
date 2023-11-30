a5802fa91e1bafc953fca097af91c3eefdb183ba
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-11-30 17:31:40 +0200

    Update app.js

--- DOM Manipulation and Events/Lab/04. Mouse-Gradient/app.js
+++ DOM Manipulation and Events/Lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,11 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    const gradientElement = document.getElementById('gradient');
+    gradientElement.addEventListener('mouseover', (event) => {
+        let power = event.offsetX / event.target.clientWidth;
+        power = Math.trunc(power * 100);
+        document.getElementById('result').textContent = power.toString();
+    });
+    gradientElement.addEventListener('mouseout', (event) => {
+        document.getElementById('result').textContent = "";
+    });
 }
