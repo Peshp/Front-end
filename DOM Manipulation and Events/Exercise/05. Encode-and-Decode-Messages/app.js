81f7385f5f03fedbd37182a80bc2e2bf26e09ff9
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-12-01 18:53:19 +0200

    Update app.js

--- DOM Manipulation and Events/Exercise/05. Encode-and-Decode-Messages/app.js
+++ DOM Manipulation and Events/Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,28 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    const [encodeBtn, decodeBtn] = document.getElementsByTagName('button');
+    const [encodeText, decodeText] = document.getElementsByTagName('textarea');
+
+    encodeBtn.addEventListener('click', (e) => {
+        let chars = encodeText.value;
+        let result = "";
+
+        for (let i = 0; i < chars.length; i++) {
+           let ascii = chars[i].charCodeAt(0);
+           result += String.fromCharCode(ascii + 1);
+        }
+
+        decodeText.textContent = result;
+    });
+
+    decodeBtn.addEventListener('click', (e) => {
+        let chars = decodeText.value;
+        let result = "";
+
+        for (let i = 0; i < chars.length; i++) {
+            let ascii = chars[i].charCodeAt(0);
+            result += String.fromCharCode(ascii - 1);
+        }
+
+        decodeText.textContent = result;
+    });
 }
