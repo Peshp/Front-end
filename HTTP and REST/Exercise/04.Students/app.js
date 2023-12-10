cbd080242862cd9469dc0344826fd71ab656de20
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-12-10 15:16:27 +0200

    updates

--- HTTP and REST/Exercise/04.Students/app.js
+++ HTTP and REST/Exercise/04.Students/app.js
@@ -1,5 +1,10 @@
 function attachEvents() {
-  // TODO:
+  const baseUrl = 'http://localhost:3030/jsonstore/collections/students';
+  const labels = document.getElementsByClassName('labels');
+  const inputs = document.getElementsByClassName('inputs')
+  const submitBtn = document.getElementById('submit');
+
+  
 }
 
 attachEvents();
