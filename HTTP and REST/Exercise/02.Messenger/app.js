5d7e26485a1301434a23f2d696a0d404eec47484
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-12-10 15:14:56 +0200

    Update app.js

--- HTTP and REST/Exercise/02.Messenger/app.js
+++ HTTP and REST/Exercise/02.Messenger/app.js
@@ -1,5 +1,35 @@
 function attachEvents() {
-    // TODO:
+    const baseUrl = 'http://localhost:3030/jsonstore/messenger';
+    const messages = document.getElementById('messages');
+    const [nameInput, contentInput, sendButton, refreshButton] = document.getElementsByTagName("input");
+
+    sendButton.addEventListener('click', async () => {
+        const newMessage = {
+            author: nameInput.value,
+            content: contentInput.value,
+        };
+
+        await fetch(baseUrl, {
+            method: 'POST',
+            body: JSON.stringify(newMessage),
+        });
+
+        nameInput.value = '';
+        contentInput.value = '';
+    })
+
+    refreshButton.addEventListener('click', async () => {
+        let allMessages = [];
+
+        let res = await fetch(baseUrl);
+        let oneMessage = await res.json();
+
+        for (const item of Object.values(oneMessage)) {
+            allMessages.push(`${item.author}: ${item.content}`);
+        }
+
+        messages.textContent = allMessages.join('\n');
+    })
 }
 
 attachEvents();
