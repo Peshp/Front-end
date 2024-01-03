60c13f08f76dc5fa51ae2dc59c82be30ab15e35c
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2024-01-03 23:31:48 +0200

    Create solve.js

---
+++ typescript/01.Data Class/solve.js
@@ -0,0 +1,21 @@
+var Data = /** @class */ (function () {
+    function Data(method, url, version, message) {
+        this.Method = method;
+        this.Url = url;
+        this.Version = version;
+        this.Message = message;
+        this.Response = 'undefined';
+        this.Fulfilled = false;
+    }
+    Data.prototype.toString = function () {
+        return "method: '".concat(this.Method, "'\n") +
+            "url: '".concat(this.Url, "']\n") +
+            "version: '".concat(this.Version, "'\n") +
+            "message: '".concat(this.Message, "'\n") +
+            "response: '".concat(this.Message, "'\n") +
+            "fulfilled: '".concat(this.Fulfilled, "'");
+    };
+    return Data;
+}());
+var myData = new Data('GET', 'http://google.com', 'HTTP/1.1', '');
+console.log(myData.toString);
