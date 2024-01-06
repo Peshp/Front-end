d66a35cdf245289be668d55cf3de2147eb800e8d
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2024-01-06 23:17:35 +0200

    Create Angular app

---
+++ Angular/Angular Components Lab - Articles/article-app/src/app/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ArticlesComponent } from '../articles/articles.component';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ArticlesComponent
+  ]
+})
+export class AppModule { }
--- Angular/Angular Components Lab - Articles/article-app/src/app/article/article.component.ts
+++ Angular/Angular Components Lab - Articles/article-app/src/app/article/article.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Article } from '../models/article-model';
 
 @Component({
   selector: 'app-article',
@@ -7,6 +8,43 @@ import { Component } from '@angular/core';
   templateUrl: './article.component.html',
   styleUrl: './article.component.scss'
 })
-export class ArticleComponent {
 
+export class ArticleComponent {
+  constructor(){
+    this.articleDescLen = 0;
+    this.descToShow = "";
+  }
+
+  private symbols: number = 250;
+  @Input() article: Article | undefined;
+  @Input() articleDesc: string | undefined;
+  descToShow: string | undefined;
+  articleDescLen: number;
+  showReadMoreBtn: boolean = true;
+  showHideBtn: boolean = false;
+  imageIsShown: boolean = false;
+  imageButtonTitle: string = "Show Image";
+
+  readMore(){
+    this.articleDescLen += this.symbols;
+    if(this.articleDescLen >= this.articleDesc.length){
+      this.showHideBtn = true;
+      this.showReadMoreBtn = false;
+    } else{
+      this.descToShow = this.articleDesc?.substring(0, this.articleDescLen);
+    }
+  }
+
+  toggleImage(){
+    this.imageIsShown = !this.imageIsShown;
+    this.imageButtonTitle = this.imageButtonTitle === 'Show Image'
+      ? 'Hide Image' : "Show Image";
+  }
+
+  hideDesc(){
+    this.descToShow = "";
+    this.articleDescLen = 0;
+    this.showHideBtn = false;
+    this.showReadMoreBtn = true;
+  }
 }
