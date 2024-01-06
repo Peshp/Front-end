20f741f961755459dde36dd03d7195c3e334271f
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2024-01-06 11:08:00 +0200

    Create Melon.ts

---
+++ typescript/04.The Elemelons/Melon.ts
@@ -0,0 +1,9 @@
+abstract class Melon{
+    constructor(weight:number, melonSort: string){
+        this.weight = weight;
+        this.melonSort = melonSort;
+    }
+
+    weight: number;
+    melonSort: string;
+}
