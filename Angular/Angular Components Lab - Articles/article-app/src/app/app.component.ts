7ae490bac9a2362125969cdd95df845f4ba4e14f
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2024-01-07 18:05:36 +0200

    Update app.component.ts

--- Angular/Angular Components Lab - Articles/article-app/src/app/app.component.ts
+++ Angular/Angular Components Lab - Articles/article-app/src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ArticleComponent } from './article/article.component';
+import { ArticlesComponent } from './articles/articles.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ArticleComponent, ArticlesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Article site';
+  title = 'Article App';
 }
